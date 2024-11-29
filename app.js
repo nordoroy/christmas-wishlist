6166c079476c546e55aa758e704c42f407f88a21
Ron Gürber <ron.guerber@students.fhnw.ch>
2024-11-29 18:37:39 +0100

    added multiple inputfields

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import { getFirestore, collection, addDoc, serverTimestamp, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-
   apiKey: "AIzaSyAjk9lfkW2Pt2K38fRvTyZKNzIyFY2M0O4",
-
   authDomain: "christmas-wishlist-e4d46.firebaseapp.com",
-
   projectId: "christmas-wishlist-e4d46",
-
   storageBucket: "christmas-wishlist-e4d46.firebasestorage.app",
-
   messagingSenderId: "74340929706",
-
   appId: "1:74340929706:web:d53d686bc7d85212d980d3",
-
   measurementId: "G-P71EZG24QD"
-
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
@@ -42,12 +24,23 @@ const dataList = document.getElementById('dataList');
 // Submit data to Firestore
 dataForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const userInput = document.getElementById('userInput').value;
+    const nameInput = document.getElementById('nameInput').value.trim();
+    const stringInput = document.getElementById('wishInput').value.trim();
+    const urlInput = document.getElementById('urlInput').value.trim();
+    if (!nameInput || !stringInput) {
+        alert("Bitte Name und Wunsch eingeben");
+        return;
+    }
+    const data = {
+        name: nameInput,
+        content: wishInput,
+        timestamp: serverTimestamp()
+    };
+    if (urlInput) {
+        data.url = urlInput;
+    };
     try {
-        await addDoc(collection(db, 'sharedData'), {
-            content: userInput,
-            timestamp: serverTimestamp()
-        });
+        await addDoc(collection(db, 'sharedData'), data);
         dataForm.reset();
     } catch (error) {
         console.error("Error adding document: ", error);
@@ -59,8 +52,15 @@ const q = query(collection(db, 'sharedData'), orderBy('timestamp', 'desc'));
 onSnapshot(q, (snapshot) => {
     dataList.innerHTML = '';
     snapshot.forEach((doc) => {
+        const data = doc.data();
         const li = document.createElement('li');
-        li.textContent = doc.data().content;
+
+        let content = `<strong>${data.name}</strong>: ${data.content}`;
+        if(data.url) {
+            content += ` - <a href="${data.url}" target="_blank">Link</a>`;
+        }
+
+        li.innerHTML = content;
         dataList.appendChild(li);
     });
 });
