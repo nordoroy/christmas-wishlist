dc975498d31401a08d85d9b5fc12e64563aac0ea
Ron Gürber <ron.guerber@students.fhnw.ch>
2024-11-29 19:31:47 +0100

    functional storage with URL

--- app.js
+++ app.js
@@ -31,9 +31,23 @@ dataForm.addEventListener('submit', async (e) => {
         alert("Bitte Name und Wunsch eingeben");
         return;
     }
+    if (urlInput) {
+        // Simple URL validation
+        const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR IP (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+          '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+        if (!urlPattern.test(urlInput)) {
+            alert("Please enter a valid URL or leave the URL field empty.");
+            return;
+        }
+    }
     const data = {
         name: nameInput,
-        content: wishInput,
+        content: stringInput,
         timestamp: serverTimestamp()
     };
     if (urlInput) {
